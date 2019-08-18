1d2118c5b315990c4a6bb319b40d7c0da0e9b3c4
Jayant Kumar <jaykrs@gmail.com>
2019-08-18 16:24:40 +0530

    alpha

---
+++ askert/App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { SwitchNavigator } from 'react-navigation';
+import Signin from './components/Signin/Signin';
+import Signup from './components/Signup/Signup';
+import Confirmation from './components/Confirmation/Confirmation';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+const MainNavigator = SwitchNavigator({
+  Signin: { screen: Signin },
+  Signup: { screen: Signup },
+  // Profile: { screen: Profile },
+  Confirmation: { screen: Confirmation },
+  ForgotPassword: { screen: ForgotPassword },
+  initialRouteName: 'Login',
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MainNavigator/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ askert/src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+  identityPoolId: 'jaykrsCognitoDemo',
+  region: 'mumbai',
+  userPoolId: 'ap-south-1_I0ZGh5WCN',
+  userPoolWebClientId: '1m2o17patijsohfq9b1rdjs1ce'
+}
+
+export default awsConfig;
